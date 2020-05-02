72cbc28de6286e272a5184eb26232228e2e4ae1d
Daniel Angst <dcangst@gmail.com>
2020-05-02 16:33:16 +0200

    minor updates to text

--- app/ui.R
+++ app/ui.R
@@ -6,10 +6,10 @@ dashboardPage(title = "COVID-19 Reff",
   dashboardHeader(title = HTML("COVID-19 R<sub>effective</sub>")),
   dashboardSidebar(
     sidebarMenu(id = "tabs",
-      menuItem(HTML("R<sub>0</sub> in Switzerland"), tabName = "interactive", icon = icon("chart-area")),
+      menuItem(HTML("R in Switzerland"), tabName = "interactive", icon = icon("chart-area")),
       menuItem(HTML("Additional plots"),
         tabName = "overview", startExpanded = FALSE,
-        menuSubItem(HTML("R<sub>0</sub> by canton"), selected = FALSE, tabName = "overview", icon = icon("chart-area")),
+        menuSubItem(HTML("R by canton"), selected = FALSE, tabName = "overview", icon = icon("chart-area")),
         selectInput("canton", "Cantons", choices = "CH", selected = "CH",
           multiple = TRUE, selectize = TRUE, width = NULL, size = NULL),
         actionLink("selectAllCantons", "select all")
@@ -21,7 +21,7 @@ dashboardPage(title = "COVID-19 Reff",
   dashboardBody(
     tabItems(
       tabItem(tabName = "interactive",
-        box(title = HTML("Estimating effective R<sub>0</sub> in Switzerland"), width = 12,
+        box(title = HTML("Estimating effective R in Switzerland"), width = 12,
           plotlyOutput("CHinteractivePlot", height = "700px")
         ),
         fluidRow(
@@ -38,7 +38,7 @@ dashboardPage(title = "COVID-19 Reff",
       tabItem(tabName = "overview",
         fluidRow(
           column(width = 12,
-            box(title = HTML("effective R<sub>0</sub> by canton"), width = 12, height = 2600,
+            box(title = HTML("effective R by canton"), width = 12, height = 2600,
               fluidRow(
                 column(width = 4,
                   plotOutput("cumulativePlot") %>% withSpinner()
