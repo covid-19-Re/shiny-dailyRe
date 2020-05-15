f05c494b9b9d20ddc99f894efe4876f95e74c904
Jérémie Scire <jeremie.scire@bsse.ethz.ch>
2020-05-15 13:58:07 +0200

    Add manual steps in step-wise Re Squashed commit of the following:
    commit b4f368f3899b9409fd059e9fc3ce2e1516fc3e32
    Author: Jérémie Scire <jeremie.scire@bsse.ethz.ch>
    Date:   Fri May 15 13:55:41 2020 +0200
    
        Include additional interval ends

--- app/otherScripts/3_doReEstimates.R
+++ app/otherScripts/3_doReEstimates.R
@@ -35,7 +35,7 @@ estimateRe <- function(
   windowLength= 4,
   mean_si = 4.8,
   std_si  = 2.3) {
-
+  
   offset <- 1
   cumulativeIncidence <- 0
   while (cumulativeIncidence < minimumCumul) {
@@ -47,16 +47,16 @@ estimateRe <- function(
   }
   ## offset needs to be at least two for EpiEstim
   offset <- max(2, offset)
-
+  
   rightBound <- length(incidenceData) - (windowLength - 1)
-
+  
   if (rightBound < offset) { ## no valid data point, return empty estimate
     return(data.frame(date = c(), variable = c(), value = c(), estimate_type = c()))
   }
-
+  
   ## generate start and end bounds for Re estimates
   if (variationType == "step") {
-
+    
     # index in incidenceData that corresponds to the interval_end date
     interval_end_indices <- sapply(
       interval_ends,
@@ -68,7 +68,7 @@ estimateRe <- function(
     #starts and end indices of the intervals (numeric vector)
     t_start <- c(offset, na.omit(interval_end_indices) + 1)
     t_end <- c(na.omit(interval_end_indices), length(incidenceData))
-
+    
     if (offset >= length(incidenceData)) {
       return(data.frame(date = c(), variable = c(), value = c(), estimate_type = c()))
     }
@@ -80,15 +80,15 @@ estimateRe <- function(
       t_start[1] <- offset
       t_end <- t_end[-1]
     }
-
+    
     # make sure there are no intervals beyond the length of the data
     while (t_start[length(t_start)] >= length(incidenceData)) {
       t_end <- t_end[-length(t_end)]
       t_start <- t_start[-length(t_start)]
     }
-
+    
     outputDates <- dates[t_start[1]:t_end[length(t_end)]]
-
+    
   } else if (variationType == "slidingWindow") {
     # computation intervals corresponding to every position of the
     # sliding window
@@ -99,10 +99,10 @@ estimateRe <- function(
     print("Unknown time variation.")
     return(data.frame(date = c(), variable = c(), value = c(), estimate_type = c()))
   }
-
+  
   ## offset dates to account for delay between infection and recorded event (testing, hospitalization, death...)
   outputDates <- outputDates - estimateOffsetting
-
+  
   if (method == "Cori") {
     R_instantaneous <- estimate_R(
       incidenceData,
@@ -118,40 +118,40 @@ estimateRe <- function(
   } else if (method == "WallingaTeunis") {
     R_instantaneous <- wallinga_teunis(
       incidenceData,
-        method = "parametric_si",
-        config = list(
-          mean_si = mean_si, std_si = std_si,
-          t_start = t_start,
-          t_end = t_end,
-          n_sim = 10)
-        )
+      method = "parametric_si",
+      config = list(
+        mean_si = mean_si, std_si = std_si,
+        t_start = t_start,
+        t_end = t_end,
+        n_sim = 10)
+    )
   } else {
     print("Unknown estimation method")
     return(data.frame(date = c(), variable = c(), value = c(), estimate_type = c()))
   }
-
+  
   if (variationType == "step") {
     R_mean <- unlist(lapply(seq_along(t_start),
-      function(x) {
-        rep(R_instantaneous$R$`Mean(R)`[x], t_end[x] - t_start[x] + 1)
-      }
+                            function(x) {
+                              rep(R_instantaneous$R$`Mean(R)`[x], t_end[x] - t_start[x] + 1)
+                            }
     ))
     R_highHPD <- unlist(lapply(seq_along(t_start),
-      function(x) {
-        rep(R_instantaneous$R$`Quantile.0.975(R)`[x], t_end[x] - t_start[x] + 1)
-      }
+                               function(x) {
+                                 rep(R_instantaneous$R$`Quantile.0.975(R)`[x], t_end[x] - t_start[x] + 1)
+                               }
     ))
     R_lowHPD <- unlist(lapply(seq_along(t_start),
-      function(x) {
-        rep(R_instantaneous$R$`Quantile.0.025(R)`[x], t_end[x] - t_start[x] + 1)
-      }
+                              function(x) {
+                                rep(R_instantaneous$R$`Quantile.0.025(R)`[x], t_end[x] - t_start[x] + 1)
+                              }
     ))
   } else {
     R_mean <- R_instantaneous$R$`Mean(R)`
     R_highHPD <- R_instantaneous$R$`Quantile.0.975(R)`
     R_lowHPD <- R_instantaneous$R$`Quantile.0.025(R)`
   }
-
+  
   if (rightTruncation > 0) {
     if (rightTruncation >= length(outputDates)) {
       return(data.frame(date = c(), variable = c(), value = c(), estimate_type = c()))
@@ -162,7 +162,7 @@ estimateRe <- function(
     R_highHPD <- R_highHPD[-seq(originalLength, by = -1, length.out = rightTruncation)]
     R_lowHPD <- R_lowHPD[-seq(originalLength, by = -1, length.out = rightTruncation)]
   }
-
+  
   if (leftTruncation > 0) {
     if (leftTruncation >= length(outputDates)) {
       return(data.frame(date = c(), variable = c(), value = c(), estimate_type = c()))
@@ -173,17 +173,17 @@ estimateRe <- function(
     R_highHPD <- R_highHPD[-seq(1, leftTruncation)]
     R_lowHPD <- R_lowHPD[-seq(1, leftTruncation)]
   }
-
+  
   result <- data.frame(
     date = outputDates,
     R_mean = R_mean,
     R_highHPD = R_highHPD,
     R_lowHPD = R_lowHPD)
-
+  
   result <- melt(result, id.vars = "date")
   colnames(result) <- c("date", "variable", "value")
   result$estimate_type <- paste0(method, "_", variationType)
-
+  
   return(result)
 }
 
@@ -195,19 +195,19 @@ doReEstimation <- function(
   interval_ends = c("2020-04-01"),
   delays,
   truncations) {
-
+  
   end_result <-  data.frame()
-
+  
   for (method_i in methods) {
     for (variation_i in variationTypes) {
       incidence_data <- data_subset$value[data_subset$variable == "incidence"]
       dates <- data_subset$date[data_subset$variable == "incidence"]
-
+      
       offsetting <- delays[method_i]
-
+      
       leftTrunc <- truncations$left[method_i]
       rightTrunc <- truncations$right[method_i]
-
+      
       result <- estimateRe(
         dates = dates,
         incidenceData = incidence_data,
@@ -232,7 +232,7 @@ doReEstimation <- function(
       }
     }
   }
-
+  
   return(end_result)
 }
 
@@ -241,11 +241,11 @@ getIntervalEnds <- function(
   interval_ends,
   region_i,
   swissRegions = c("Liechtenstein", "AG", "AI", "AR", "BE", "BL", "BS", "FR", "GE", "GL",
-    "GR", "grR Central Switzerland", "grR Eastern Switzerland",
-    "grR Espace Mittelland", "grR Lake Geneva Region", "grR Northwestern Switzerland",
-    "grR Ticino", "grR Zurich", "JU", "LU", "NE", "NW", "OW", "SG",
-    "SH", "SO", "SZ", "TG", "TI", "UR", "VD", "VS", "ZG", "ZH")) {
-
+                   "GR", "grR Central Switzerland", "grR Eastern Switzerland",
+                   "grR Espace Mittelland", "grR Lake Geneva Region", "grR Northwestern Switzerland",
+                   "grR Ticino", "grR Zurich", "JU", "LU", "NE", "NW", "OW", "SG",
+                   "SH", "SO", "SZ", "TG", "TI", "UR", "VD", "VS", "ZG", "ZH")) {
+  
   if ("data.frame" %in% class(interval_ends)) {
     if (region_i %in% swissRegions) {
       region_i <- "Switzerland"
@@ -254,7 +254,7 @@ getIntervalEnds <- function(
       filter(region == region_i,
              type == "start",
              measure != "testing")
-
+    
     region_interval_ends <- sort(unique(pull(interventionDataSubset, "date")))
   } else {
     region_interval_ends <- interval_ends
@@ -263,6 +263,27 @@ getIntervalEnds <- function(
     region_interval_ends <- c(Sys.Date())
   }
   return(region_interval_ends)
+}
+
+addCustomIntervalEnds <- function(region_interval_ends,
+                                  additional_interval_ends,
+                                  region_i,
+                                  data_type_i,
+                                  swissRegions = c("Liechtenstein", "AG", "AI", "AR", "BE", "BL", "BS", "FR", "GE", "GL",
+                                                   "GR", "grR Central Switzerland", "grR Eastern Switzerland",
+                                                   "grR Espace Mittelland", "grR Lake Geneva Region", "grR Northwestern Switzerland",
+                                                   "grR Ticino", "grR Zurich", "JU", "LU", "NE", "NW", "OW", "SG",
+                                                   "SH", "SO", "SZ", "TG", "TI", "UR", "VD", "VS", "ZG", "ZH")) {
+  
+  if (region_i %in% swissRegions) {
+    region_i <- "Switzerland"
+  }
+  interventionDataSubset <- additional_interval_ends %>%
+    filter(region == region_i &
+           (data_type == data_type_i | data_type == "all"))
+  
+  all_interval_ends <- sort(unique(c(region_interval_ends, pull(interventionDataSubset, "date"))))
+  return(all_interval_ends)
 }
 
 
@@ -278,18 +299,20 @@ doAllReEstimations <- function(
   variationTypes = c("step", "slidingWindow"),
   all_delays,
   truncations,
-  interval_ends = c("2020-04-01"), ...) {
-
+  interval_ends = c("2020-04-01"),
+  additional_interval_ends,
+  ...) {
+  
   results_list <- list()
-
+  
   for (source_i in unique(data$source)) {
     cat("estimating Re for data source: ", source_i, "...\n")
     for (region_i in unique(data$region)) {
       cat("  Region: ", region_i, "\n")
-
+      
       ## take region specific interval_ends
       region_interval_ends <- getIntervalEnds(interval_ends, region_i, ...)
-
+      
       ## Run EpiEstim
       for (data_type_i in unique(data$data_type)) {
         subset_data <- subset(data, region == region_i & source == source_i & data_type == data_type_i)
@@ -297,29 +320,33 @@ doAllReEstimations <- function(
           next
         }
         cat("    Data type: ", data_type_i, "\n")
-
+        
         delay_i <- all_delays[[data_type_i]]
-
+        all_interval_ends <- addCustomIntervalEnds(region_interval_ends,
+                                                   additional_interval_ends,
+                                                   region_i,
+                                                   data_type_i)
+        
         for (replicate_i in unique(unique(subset_data$replicate))) {
           subset_data_rep <- subset(subset_data, subset_data$replicate == replicate_i)
           results_list <- c(results_list,
-            list(
-              doReEstimation(
-                subset_data_rep,
-                slidingWindow = slidingWindow,
-                methods = methods,
-                variationTypes = variationTypes,
-                interval_ends = region_interval_ends,
-                delays = delay_i,
-                truncations = truncations
-              )
-            )
+                            list(
+                              doReEstimation(
+                                subset_data_rep,
+                                slidingWindow = slidingWindow,
+                                methods = methods,
+                                variationTypes = variationTypes,
+                                interval_ends = all_interval_ends,
+                                delays = delay_i,
+                                truncations = truncations
+                              )
+                            )
           )
         }
       }
     }
   }
-
+  
   return(rbind.fill(results_list))
 }
 
@@ -341,20 +368,28 @@ load(file = pathToSampledInfectDataSave)
 ### Date input
 
 pathToInterventionDates <- here::here("../covid19-additionalData/interventions/", "interventions.csv")
+pathToAdditionalIntervalEndDates <- here::here("../covid19-additionalData/interventions/","additional_interval_ends.csv")
 
 interventionData <- read_csv(pathToInterventionDates,
-  col_types = cols(
-    date = col_date(format = ""),
-    country = col_character(),
-    measure = col_character(),
-    source = col_character(),
-    name = col_character(),
-    y = col_double(),
-    text = col_character(),
-    tooltip = col_character(),
-    type = col_character(),
-    plotTextPosition = col_character(),
-    region = col_character()))
+                             col_types = cols(
+                               date = col_date(format = ""),
+                               country = col_character(),
+                               measure = col_character(),
+                               source = col_character(),
+                               name = col_character(),
+                               y = col_double(),
+                               text = col_character(),
+                               tooltip = col_character(),
+                               type = col_character(),
+                               plotTextPosition = col_character(),
+                               region = col_character()))
+
+additionalIntervalEnds <-  read_csv(pathToAdditionalIntervalEndDates,
+                                    col_types = cols(
+                                      date = col_date(format = ""),
+                                      region = col_character(),
+                                      data_type = col_character(),
+                                      explanation = col_character()))
 
 interval_ends <- interventionData
 
@@ -384,13 +419,14 @@ truncations <- list(
 
 ### Run EpiEstim
 estimatesReRaw_calc <- doAllReEstimations(
-    subset(sampledInfectData, variable == "incidence"),
-    slidingWindow = window,
-    methods = "Cori",
-    all_delays = all_delays,
-    truncations = truncations,
-    interval_ends = interval_ends,
-    swissRegions = swissRegions)
+  subset(sampledInfectData, variable == "incidence"),
+  slidingWindow = window,
+  methods = "Cori",
+  all_delays = all_delays,
+  truncations = truncations,
+  interval_ends = interval_ends,
+  additional_interval_ends = additionalIntervalEnds,
+  swissRegions = swissRegions)
 
 library(tidyverse)
 estimatesReRaw <- as_tibble(estimatesReRaw_calc) %>%
@@ -398,7 +434,7 @@ estimatesReRaw <- as_tibble(estimatesReRaw_calc) %>%
     data_type = factor(
       data_type,
       levels = c("infection_Confirmed cases", "infection_Hospitalized patients",
-        "infection_Deaths", "infection_Excess deaths"),
+                 "infection_Deaths", "infection_Excess deaths"),
       labels = c("Confirmed cases", "Hospitalized patients", "Deaths", "Excess deaths")))
 
 save(estimatesReRaw, file = pathToEstimatesReRaw)
