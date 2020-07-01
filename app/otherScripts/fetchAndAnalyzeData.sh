38c503465a014154c5d7469b2bbe71eb04c3c852
Daniel Angst <dcangst@gmail.com>
2020-07-01 08:36:29 +0200

    nicer logging

--- app/otherScripts/fetchAndAnalyzeData.sh
+++ app/otherScripts/fetchAndAnalyzeData.sh
@@ -4,11 +4,15 @@ parent_path=$(
   pwd -P
 )
 
-checkScriptStatus () {
+runRScript () {
+  echo "running" $1 "..."
+  Rscript --vanilla --verbose $1 >>messages.Rout 2>>errors.Rout
   retVal=$?
   if [ $retVal -ne 0 ]; then
     echo "Script didn't run successfully (Error" $retVal ")"
     exit 1
+  else
+    echo "       " $1 "done"
   fi
 }
 
@@ -35,25 +39,22 @@ echo "updating BAG Data (polybox sync)"
 owncloudcmd -n -s ../data/BAG \
   https://polybox.ethz.ch/remote.php/webdav/BAG%20COVID19%20Data
 echo "running R script to extract BAG data ..."
-Rscript --vanilla --verbose format_BAG_data.R >>messages.Rout 2>>errors.Rout
-checkScriptStatus
+runRScript format_BAG_data.R
 echo "running R data analysis scripts ..."
-Rscript --vanilla --verbose 1_getRawData.R >>messages.Rout 2>>errors.Rout
-checkScriptStatus
-Rscript --vanilla --verbose 2_getInfectionIncidence.R >>messages.Rout 2>>errors.Rout
-checkScriptStatus
-Rscript --vanilla --verbose 3_doReEstimates.R >>messages.Rout 2>>errors.Rout
-checkScriptStatus
+runRScript 1_getRawData.R
+runRScript 2_getInfectionIncidence.R
+runRScript 3_doReEstimates.R
 # summarize data in seperate process to avoid C stack limit
-Rscript --vanilla --verbose 4_doReEstimatesSum.R >>messages.Rout 2>>errors.Rout
-checkScriptStatus
-Rscript --vanilla --verbose 5_makeReffPlotly.R >>messages.Rout 2>>errors.Rout
-checkScriptStatus
+runRScript 4_doReEstimatesSum.R
+runRScript 5_makeReffPlotly.R
+
 # copy data
 cp -f ../data/temp/* ../data/
+
 # make app restart on next connection
 touch ../restart.txt
 echo "fetchAndAnalzeData.sh done ..."
+
 # crontab settings
 # run every 3h starting at 1am
 # 0 1,4,7,10,13,16,19,22 * * * /home/covid-19-re/covid-19-re-shiny-app/app/otherScripts/fetchAndAnalyzeData.sh
