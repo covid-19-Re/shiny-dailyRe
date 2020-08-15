50c62f35085681bebd671c8423e5ce65810a30e6
Daniel Angst <dcangst@gmail.com>
2020-08-15 11:11:26 +0200

    fix wrong comparison plots

--- app/server.R
+++ app/server.R
@@ -88,9 +88,20 @@ server <- function(input, output, session) {
             .groups = "keep"
           )
 
-        caseData <- caseData %>%
-          left_join(deconvolutedData, by = c("country", "region", "source", "data_type", "date")) %>%
-          arrange(countryIso3, region, source, data_type, date)
+        if(nrow(caseData %>% filter(date_type == "report_plotting")) > 0) {
+    
+          caseData <- caseData %>%
+            filter(date_type == "report_plotting") %>% 
+            left_join(deconvolutedData, by = c("country", "region", "source", "data_type", "date")) %>%
+            arrange(countryIso3, region, source, data_type, date)
+          
+        } else {
+          caseData <- caseData %>%
+            dplyr::group_by(date, region, country, countryIso3, source, data_type, populationSize, continent) %>% 
+            dplyr::summarise(value = sum(value), .groups = "drop") %>% # there should only be one "date_type" but the summing is left in there in case.
+            left_join(deconvolutedData, by = c("country", "region", "source", "data_type", "date")) %>%
+            arrange(countryIso3, region, source, data_type, date)
+        }
 
         testsPath <- file.path(pathToCountryData, "Europe", str_c(icountry, "-Tests.rds"))
         if (file.exists(testsPath)) {
@@ -142,9 +153,20 @@ server <- function(input, output, session) {
             .groups = "keep"
           )
 
-        caseData <- caseData %>%
-          left_join(deconvolutedData, by = c("country", "region", "source", "data_type", "date")) %>%
-          arrange(countryIso3, region, source, data_type, date)
+        if(nrow(caseData %>% filter(date_type == "report_plotting")) > 0) {
+    
+          caseData <- caseData %>%
+            filter(date_type == "report_plotting") %>% 
+            left_join(deconvolutedData, by = c("country", "region", "source", "data_type", "date")) %>%
+            arrange(countryIso3, region, source, data_type, date)
+          
+        } else {
+          caseData <- caseData %>%
+            dplyr::group_by(date, region, country, countryIso3, source, data_type, populationSize, continent) %>% 
+            dplyr::summarise(value = sum(value), .groups = "drop") %>% # there should only be one "date_type" but the summing is left in there in case.
+            left_join(deconvolutedData, by = c("country", "region", "source", "data_type", "date")) %>%
+            arrange(countryIso3, region, source, data_type, date)
+        }
 
         testsPath <- file.path(pathToCountryData, "Africa", str_c(icountry, "-Tests.rds"))
         if (file.exists(testsPath)) {
