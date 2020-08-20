a7f865257659f6330563b5108a27fa51fd2312df
Daniel Angst <dcangst@gmail.com>
2020-08-20 18:02:09 +0200

    only load country shapes when needed

--- app/global.R
+++ app/global.R
@@ -53,38 +53,3 @@ countryList <- tibble(
     dsn = "data/geoData/",
     layer = "ne_50m_admin_0_countries",
     stringsAsFactors = FALSE)
-
-  CHEregionsShape <- rgdal::readOGR(
-    dsn = "data/geoData/",
-    layer = "swissBOUNDARIES3D_1_3_TLM_KANTONSGEBIET",
-    stringsAsFactors = FALSE) %>%
-    sp::spTransform(countriesShape@proj4string)
-
-  cantonNames <- tibble(
-    region = c(
-      "ZH", "BE", "LU", "UR", "SZ",
-      "OW", "NW", "GL", "ZG", "FR",
-      "SO", "BS", "BL", "SH", "AR",
-      "AI", "SG", "GR", "AG", "TG",
-      "TI", "VD", "VS", "NE", "GE", "JU"),
-    KANTONSNUM = c(
-      1:26
-    )
-  )
-
-  CHEregionsShape@data$KANTONSNUM <- as.integer(CHEregionsShape@data$KANTONSNUM)
-
-  CHEregionsShape@data <- left_join(
-    CHEregionsShape@data,
-    cantonNames,
-    by = "KANTONSNUM"
-  )
-  CHEregionsShape@data$ADM0_A3_IS <- "CHE"
-
-  ZAFregionsShape <- rgdal::readOGR(
-    dsn = "data/geoData/",
-    layer = "zaf_admbnda_adm1_2016SADB_OCHA",
-    stringsAsFactors = FALSE)
-  ZAFregionsShape@data$ADM0_A3_IS <- "ZAF"
-  ZAFregionsShape@data$region <- ZAFregionsShape@data$ADM1_EN %>% recode("Nothern Cape" = "Northern Cape")
-  ZAFregionsShape@data$NAME <- ZAFregionsShape@data$region
--- app/server.R
+++ app/server.R
@@ -165,6 +165,11 @@ server <- function(input, output, session) {
 
   output$mapPlot <- renderLeaflet({
     worldMapData <- worldMapData()
+    
+    countriesShape <- rgdal::readOGR(
+      dsn = "data/geoData/",
+      layer = "ne_50m_admin_0_countries",
+      stringsAsFactors = FALSE)
 
     countriesShape@data <- left_join(
       countriesShape@data,
@@ -187,6 +192,33 @@ server <- function(input, output, session) {
       addMapPane("region", zIndex = 420) 
 
     if ("CHE" %in% input$regionCountrySelect) {
+      CHEregionsShape <- rgdal::readOGR(
+        dsn = "data/geoData/",
+        layer = "swissBOUNDARIES3D_1_3_TLM_KANTONSGEBIET",
+        stringsAsFactors = FALSE) %>%
+        sp::spTransform(countriesShape@proj4string)
+
+      cantonNames <- tibble(
+        region = c(
+          "ZH", "BE", "LU", "UR", "SZ",
+          "OW", "NW", "GL", "ZG", "FR",
+          "SO", "BS", "BL", "SH", "AR",
+          "AI", "SG", "GR", "AG", "TG",
+          "TI", "VD", "VS", "NE", "GE", "JU"),
+        KANTONSNUM = c(
+          1:26
+        )
+      )
+
+      CHEregionsShape@data$KANTONSNUM <- as.integer(CHEregionsShape@data$KANTONSNUM)
+
+      CHEregionsShape@data <- left_join(
+        CHEregionsShape@data,
+        cantonNames,
+        by = "KANTONSNUM"
+      )
+      CHEregionsShape@data$ADM0_A3_IS <- "CHE"
+      
       CHEregionsShape@data <- left_join(
         CHEregionsShape@data,
         worldMapData,
@@ -209,6 +241,14 @@ server <- function(input, output, session) {
     }
 
     if ("ZAF" %in% input$regionCountrySelect) {
+        ZAFregionsShape <- rgdal::readOGR(
+          dsn = "data/geoData/",
+          layer = "zaf_admbnda_adm1_2016SADB_OCHA",
+          stringsAsFactors = FALSE)
+        ZAFregionsShape@data$ADM0_A3_IS <- "ZAF"
+        ZAFregionsShape@data$region <- ZAFregionsShape@data$ADM1_EN %>% recode("Nothern Cape" = "Northern Cape")
+        ZAFregionsShape@data$NAME <- ZAFregionsShape@data$region
+      
       ZAFregionsShape@data <- left_join(
         ZAFregionsShape@data,
         worldMapData,
