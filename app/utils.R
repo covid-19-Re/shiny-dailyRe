d01f1820feb55234eca1ac2a7e3ba569bf8a1e62
Daniel Angst <dcangst@gmail.com>
2020-09-16 11:18:55 +0200

    show fewer digits

--- app/utils.R
+++ app/utils.R
@@ -315,8 +315,8 @@ rValueBox <- function(rEstimate, text, icon, popoverId, popoverTitle, popoverTex
  
   rEstimateText <- rEstimate %>%
       glue::glue_data(
-        "<h3 style=margin-bottom:0px>{round(mean, 3)}",
-        "<span style='font-size:22px;padding-left:10px'>({round(low, 3)} - {round(high, 3)})</span></h3>",
+        "<h3 style=margin-bottom:0px>{round(mean, 2)}",
+        "<span style='font-size:22px;padding-left:10px'>({round(low, 2)} - {round(high, 2)})</span></h3>",
         "<p style=margin-top:0px>{country} ({date})</p>"
       )
   if (length(rEstimateText) > 3) {
