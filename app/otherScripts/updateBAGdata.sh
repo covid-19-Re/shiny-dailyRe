0b7e18c0f8748e27bd6da4ba562ec058f45b1e84
Daniel Angst <dcangst@gmail.com>
2020-11-04 14:08:18 +0100

    run data update with incron

--- app/otherScripts/updateBAGdata.sh
+++ app/otherScripts/updateBAGdata.sh
@@ -1,4 +1,4 @@
-echo "updating BAG Data (polybox sync)"
+
 # for development on local macOS machine (on Linux just install the owncloud cli tools)
 # symlink your polybox folder to ../data/BAG
 # i.e. ln -s 'path/to/polybox/shared/BAG COVID19 Data' 'path/to/app/data/BAG'
