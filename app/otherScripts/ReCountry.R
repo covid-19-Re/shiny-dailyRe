43133ac881492a2005786e8b2dd19d1bbf5e867c
Daniel Angst <dcangst@gmail.com>
2020-11-04 12:51:53 +0100

    add time to log

--- app/otherScripts/ReCountry.R
+++ app/otherScripts/ReCountry.R
@@ -118,7 +118,7 @@ if (dim(countryData)[1] > 0) {
     !is.null(countryData)
   
   if (condition) {
-    cat(str_c("\n", args["country"], ": New data available. Calculating Re ...\n"))
+    cat(str_c("\n", args["country"], ": ", Sys.time(), " New data available. Calculating Re ...\n"))
     # get Infection Incidence
     # load functions
     source(here::here("app/otherScripts/2_utils_getInfectionIncidence.R"))
@@ -349,8 +349,8 @@ if (dim(countryData)[1] > 0) {
     }
     
   } else {
-    cat(str_c(args["country"], ": No new data available. Skipping Re calculation.\n"))
+    cat(str_c(args["country"], ": ", Sys.time(), " No new data available. Skipping Re calculation.\n"))
   }
 } else {
-  cat(str_c(args["country"], ": No data available.\n"))
+  cat(str_c(args["country"], ": ", Sys.time(), " No data available.\n"))
 }
