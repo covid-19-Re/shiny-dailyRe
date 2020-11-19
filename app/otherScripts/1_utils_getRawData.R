43264ca1815f3ccba17a8b60f2717cf1918cf74c
Daniel Angst <dcangst@gmail.com>
2020-11-19 09:37:39 +0100

    fix stringency fn

--- app/otherScripts/1_utils_getRawData.R
+++ app/otherScripts/1_utils_getRawData.R
@@ -1535,9 +1535,9 @@ getCountryPopData <- function(tempFileName = here::here("app/data/pop_sizes.xls"
 getDataOxfordStringency <- function(
   countries = NULL,
   tempFileName = NULL, tReload = 15) {
-  
+
   url <- "https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv"
-  
+
   if (is.null(tempFileName)) {
     csvPath <- url
   } else {
@@ -1556,36 +1556,32 @@ getDataOxfordStringency <- function(
     }
     csvPath <- tempFileName
   }
-  
-  oxfordStringencyData <- try(read_csv(csvPath, col_types = cols_only(
+
+  oxfordStringencyData <- try(read_csv(csvPath, col_types = cols(
+    .default = col_double(),
+    CountryName = col_character(),
     CountryCode = col_character(),
     RegionName = col_character(),
     RegionCode = col_character(),
     Date = col_date(format = "%Y%m%d"),
-    StringencyIndex = col_double()
-  )))
-  
+    M1_Wildcard = col_character()
+  ))) %>%
+  dplyr::rename(
+    date = Date,
+    countryIso3 = CountryCode,
+    region = RegionCode
+  )
+  oxfordStringencyData$region[is.na(oxfordStringencyData$region)] <-
+  oxfordStringencyData$countryIso3[is.na(oxfordStringencyData$region)]
+
   if ("try-error" %in% class(oxfordStringencyData)) {
     warning(str_c("couldn't get Oxford Stringency Data data from ", url, "."))
     return(NULL)
   }
-  
-  longData <- oxfordStringencyData %>%
-    dplyr::transmute(
-      date = Date,
-      countryIso3 = CountryCode,
-      region = RegionCode,
-      data_type = "Stringency Index",
-      source = "BSG Covidtracker",
-      value = StringencyIndex
-    ) %>%
-    filter(!is.na(value))
-  
-  longData$region[is.na(longData$region)] <- longData$countryIso3[is.na(longData$region)]
-  
+
   if (!is.null(countries)) {
-    longData <- longData %>%
+    oxfordStringencyData <- oxfordStringencyData %>%
       filter(countryIso3 %in% countries)
   }
-  return(longData)
+  return(oxfordStringencyData)
 }
