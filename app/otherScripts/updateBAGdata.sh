ff543a834338d29cc7200ea1a6ceb2ddb5e46b26
Daniel Angst <dcangst@gmail.com>
2022-03-03 15:46:00 +0100

    switch to using rds files

--- app/otherScripts/updateBAGdata.sh
+++ app/otherScripts/updateBAGdata.sh
@@ -10,7 +10,7 @@ parent_path=$(
 )
 cd "$parent_path"
 
-latestFile=$(ls -t /home/covid-19-re/BAGdata/*/*FOPH_COVID19_data_extract.csv | head -1)
+latestFile=$(ls -t /home/covid-19-re/BAGdata/*/*FOPH_COVID19_data_extract.rds | head -1)
 if [ -f /home/covid-19-re/lastBAGFile.txt ]; then
   lastFile=$(</home/covid-19-re/lastBAGFile.txt)
 else
