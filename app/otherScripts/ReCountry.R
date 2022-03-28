e284cb57c15ff34e0b853c87f8196245822c3708
Daniel Angst <dcangst@gmail.com>
2022-03-28 09:53:47 +0200

    add regenerateTS as argument, convert args to list

--- app/otherScripts/ReCountry.R
+++ app/otherScripts/ReCountry.R
@@ -22,14 +22,25 @@ if (interactive()) {
 source(here::here("app/otherScripts/1_utils_getRawData.R"))
 source(here::here("app/otherScripts/utils.R"))
 
-args <- commandArgs(trailingOnly = TRUE)
-# testing
-if (length(args) == 0) {
-  args <- c("CHE")
-  warning(str_c("Testing mode!! Country: ", args))
+argsRaw <- commandArgs(trailingOnly = TRUE)
+# parse args
+if (length(argsRaw) == 0) {
+  args <- list("CHE", FALSE)
+  warning(str_c("Testing mode!! Country: ", args[1]))
+} else if (length(argsRaw) == 1) {
+  args <- list(argsRaw, FALSE)
+} else if (length(argsRaw) == 2) {
+  args <- list(argsRaw[1], as.logical(argsRaw[2]))
+} else {
+  stop("Incorrect number of arguments")
 }
-args <- c(args, "TRUE")
+
 names(args) <- c("country", "regenerateTS")
+if (args$regenerateTS) {
+  cat(str_c("\n", Sys.time(), " | ", args$country, ": using whole timeseries ...\n"))
+} else {
+  cat(str_c("\n", Sys.time(), " | ", args$country, ": using truncated timeseries ...\n"))
+}
 
 basePath <- here::here("app", "data", "countryData")
 if (!dir.exists(basePath)) {
@@ -58,7 +69,7 @@ popData <- bind_rows(popDataWorldBank, popDataAdditional) %>%
 
 
 # fetch stringency data
-if (args["country"] == "CHE") {
+if (args$country == "CHE") {
   stringencyData <- read_csv(
     "https://raw.githubusercontent.com/KOF-ch/economic-monitoring/master/data/ch.kof.stringency.csv",
     col_types = cols(
@@ -77,12 +88,12 @@ if (args["country"] == "CHE") {
       StringencyIndex = value
     )
 } else {
-  stringencyData <- getDataOxfordStringency(countries = args["country"],
+  stringencyData <- getDataOxfordStringency(countries = args$country,
     tempFileName = here::here(tempPath, "oxfordStringency.csv"), tReload = 300) %>%
     mutate(source = "BSG Covidtracker")
 }
 
-stringencyDataPath <- file.path(basePath, str_c(args["country"], "-OxCGRT.rds"))
+stringencyDataPath <- file.path(basePath, str_c(args$country, "-OxCGRT.rds"))
 
 if (file.exists(stringencyDataPath)) {
   stringencyDataOld <- readRDS(stringencyDataPath)
@@ -119,13 +130,14 @@ interval_ends_df <- stringencyIndex %>%
   dplyr::select(region, interval_ends)
 
 interval_ends <- split(interval_ends_df$interval_ends, interval_ends_df$region)
-interval_ends[["default"]] <- interval_ends[[args["country"]]]
+interval_ends[["default"]] <- interval_ends[[args$country]]
 # Fetch Country Data
 countryData <- getCountryData(
-  args["country"],
+  args$country,
   tempFile = here::here(tempPath, "ECDCdata.csv"),
   HMDtemp = here::here(tempPath, "HMDdata.csv"),
-  tReload = 300)
+  tReload = 300) %>%
+  filter(!is.na(region))
 
 if (dim(countryData)[1] > 0) {
   countryData <- countryData %>%
@@ -136,10 +148,10 @@ if (dim(countryData)[1] > 0) {
     bind_rows(
       mutate(stringencyIndex,
         date_type = if_else(
-          args["country"] %in% c("DEU", "HKG"), "report_plotting", "report"))
+          args$country %in% c("DEU", "HKG"), "report_plotting", "report"))
     )
   # check for changes in country data
-  countryDataPath <- file.path(basePath, str_c(args["country"], "-Data.rds"))
+  countryDataPath <- file.path(basePath, str_c(args$country, "-Data.rds"))
   if (file.exists(countryDataPath)) {
     countryDataOld <- readRDS(countryDataPath)
     # if new data is null, keep old data (can happen because of error in reading new data)
@@ -166,14 +178,14 @@ if (dim(countryData)[1] > 0) {
     !is.null(countryData)
 
   if (condition) {
-    cat(str_c("\n", Sys.time(), " | ", args["country"], ": New data available. Calculating Re ...\n"))
+    cat(str_c("\n", Sys.time(), " | ", args$country, ": New data available. Calculating Re ...\n"))
     # send notification
-    if (args["country"] %in% c("CHE") &
+    if (args$country %in% c("CHE") &
         file.exists(here::here("app/otherScripts/sendNotifications.txt"))) {
       urls <- scan(here::here("app/otherScripts/slackWebhook.txt"), what = "character")
 
       sendSlackNotification(
-        country = args["country"],
+        country = args$country,
         event = "newData", url = urls[1], eTcompletion = Sys.time() + 105 * 60, webhookUrl = urls[2])
     }
     # get Infection Incidence
@@ -232,11 +244,11 @@ if (dim(countryData)[1] > 0) {
 
     countryData <- filter(countryData, !(data_type %in% data_type0))
     # country specific data filtering
-    if (args["country"] == "ESP") {
+    if (args$country == "ESP") {
       countryData <- countryData %>%
         filter(data_type != "Deaths")
       cat("ignoring data_type Deaths\n")
-    } else if (args["country"] == "AUT") {
+    } else if (args$country == "AUT") {
       countryData <- countryData %>%
         filter(data_type != "Deaths")
       cat("ignoring data_type Deaths\n")
@@ -250,12 +262,12 @@ if (dim(countryData)[1] > 0) {
         )
 
       right_truncation <- list()
-      if (args["country"] %in% c("DEU", "HKG")) {
+      if (args$country %in% c("DEU", "HKG")) {
         right_truncation[["Confirmed cases"]] <- 0
         right_truncation[["Confirmed cases / tests"]] <- 0
         right_truncation[["Hospitalized patients"]] <- 0
         right_truncation[["Deaths"]] <- 0
-      } else if (args["country"] %in% c("CHE", "LIE")) {
+      } else if (args$country %in% c("CHE", "LIE")) {
         max_date <- max(countryData$date)
         additionalTruncation <- case_when(
           lubridate::wday(max_date) == 3 ~ 1, # 3 = Tue, exclude Sat,
@@ -284,8 +296,8 @@ if (dim(countryData)[1] > 0) {
         dplyr::select(-countryIso3, -populationSize) %>%
         ungroup()
 
-      if (args["country"] == "CHE" & !as.logical(args["regenerateTS"])) {
-        cat("Complete Data Range:\n")
+      if (args$country == "CHE" & !args$regenerateTS) {
+        cat("Complete data range:\n")
         print(range(countryData$date))
         # only calculate starting from dateCutoffAdj
         dateCutoff <- "2021-04-01"
@@ -295,7 +307,7 @@ if (dim(countryData)[1] > 0) {
         cat("calculating for truncated data range:\n")
         print(range(countryData$date))
       } else {
-        cat("calculating for complete Data Range:\n")
+        cat("calculating for complete data range:\n")
         print(range(countryData$date))
       }
       # Deconvolution
@@ -311,7 +323,7 @@ if (dim(countryData)[1] > 0) {
         n_bootstrap = 100,
         verbose = FALSE)
 
-      if (args["country"] %in% c("CHE")) {
+      if (args$country %in% c("CHE")) {
         countryDataTests <- countryData %>%
           filter(data_type == "Confirmed cases / tests")
 
@@ -325,7 +337,7 @@ if (dim(countryData)[1] > 0) {
       }
 
       deconvolvedCountryData <- bind_rows(deconvolvedData)
-      countryDataPath <- file.path(basePath, str_c(args["country"], "-DeconvolutedData.rds"))
+      countryDataPath <- file.path(basePath, str_c(args$country, "-DeconvolutedData.rds"))
       if (dim(deconvolvedCountryData)[1] == 0) {
         print("no data remaining")
       } else {
@@ -346,23 +358,23 @@ if (dim(countryData)[1] > 0) {
         ### add additional interval 7 days before last Re estimate, for country level only
         # discarding interval ends more recent than that.
         lastIntervalEnd <- deconvolvedCountryData %>%
-          filter(data_type == "infection_Confirmed cases", region == args["country"], replicate == 0) %>%
+          filter(data_type == "infection_Confirmed cases", region == args$country, replicate == 0) %>%
           slice_max(date) %>%
           pull(date)
         lastIntervalStart <- lastIntervalEnd - 7
 
         if (length(interval_ends) == 0) {
-          interval_ends[[args["country"]]] <- lastIntervalStart
+          interval_ends[[args$country]] <- lastIntervalStart
         } else {
-          interval_ends[[args["country"]]] <- c(
-            interval_ends[[args["country"]]][interval_ends[[args["country"]]] < lastIntervalStart],
+          interval_ends[[args$country]] <- c(
+            interval_ends[[args$country]][interval_ends[[args$country]] < lastIntervalStart],
             lastIntervalStart)
         }
 
-        if (args["country"] == "CHE") {
+        if (args$country == "CHE") {
           additionalRegions <- setdiff(swissRegions, names(interval_ends))
           for (iregion in additionalRegions) {
-            interval_ends[[iregion]] <- interval_ends[[args["country"]]]
+            interval_ends[[iregion]] <- interval_ends[[args$country]]
           }
         }
 
@@ -396,7 +408,7 @@ if (dim(countryData)[1] > 0) {
           swissRegions = swissRegions)
 
         gc()
-        cat("  raw estimates done for ", args["country"], "\n")
+        cat("  raw estimates done for ", args$country, "\n")
         qs::qsave(countryEstimatesRaw, file = here::here(tempPath, "countryEstimatesRaw.qs"))
         rm(deconvolvedCountryData)
         gc()
@@ -408,7 +420,7 @@ if (dim(countryData)[1] > 0) {
           )
 
         # add extra truncation of 4 days for all Swiss cantonal estimates due to consolidation
-        if (args["country"] %in% c("CHE")) {
+        if (args$country %in% c("CHE")) {
           days_truncated <- 4
           canton_list <- c("AG", "BE", "BL", "BS", "FR", "GE", "GR", "JU", "LU", "NE", "SG", "SO", "SZ", "TG", "TI",
             "VD", "VS", "ZG", "ZH", "SH", "AR", "GL", "NW", "OW", "UR", "AI")
@@ -426,9 +438,9 @@ if (dim(countryData)[1] > 0) {
 
         }
 
-        countryDataPath <- file.path(basePath, str_c(args["country"], "-Estimates.rds"))
+        countryDataPath <- file.path(basePath, str_c(args$country, "-Estimates.rds"))
 
-        if (args["country"] == "CHE" & !as.logical(args["regenerateTS"])) {
+        if (args$country == "CHE" & !args$regenerateTS) {
           previousEstimates <- readRDS(countryDataPath) %>%
             filter(date < dateCutoff)
           newEstimates <- countryEstimates %>%
@@ -443,10 +455,10 @@ if (dim(countryData)[1] > 0) {
         saveRDS(countryEstimates, file = countryDataPath)
         # Save as .csv for data upload
         readr::write_csv(countryEstimates,
-                  path = file.path(basePath, "csv", str_c(args["country"], "-estimates.csv"))
+                  path = file.path(basePath, "csv", str_c(args$country, "-estimates.csv"))
         )
         # save simpler csvs for CHE, LIE
-        if (args["country"] %in% c("CHE", "LIE")) {
+        if (args$country %in% c("CHE", "LIE")) {
           simpleCsv <- countryEstimates %>%
             filter(data_type == "Confirmed cases", estimate_type == "Cori_slidingWindow") %>%
             select(region, date, median_R_mean, median_R_highHPD, median_R_lowHPD) %>%
@@ -455,35 +467,35 @@ if (dim(countryData)[1] > 0) {
           # write to csv directory
           readr::write_csv(
             simpleCsv,
-            file = file.path(basePath, "csv", str_c(args["country"], "-confCasesSWestimates.csv"))
+            file = file.path(basePath, "csv", str_c(args$country, "-confCasesSWestimates.csv"))
           )
           if (Sys.info()["nodename"] == "ibz-shiny.ethz.ch") {
             # write to test directory
             readr::write_csv(
               simpleCsv,
-              file = str_c("/home/covid-19-re/test-dailyRe/app/www/", args["country"], "-confCasesSWestimates.csv")
+              file = str_c("/home/covid-19-re/test-dailyRe/app/www/", args$country, "-confCasesSWestimates.csv")
             )
             if (str_detect(here(), "test")) {
               # write estimates to main app for publication
               saveRDS(countryEstimates,
                 file = str_c("/home/covid-19-re/dailyRe/app/data/countryData/",
-                  args["country"], "-Estimates.rds")
+                  args$country, "-Estimates.rds")
               )
             }
           }
         }
       }
     } else {
-      cat(str_c(Sys.time(), " | ", args["country"], ": Not enough cases. Skipping Re calculation.\n"))
+      cat(str_c(Sys.time(), " | ", args$country, ": Not enough cases. Skipping Re calculation.\n"))
     }
     # send notification
-    if (args["country"] %in% c("CHE") &
+    if (args$country %in% c("CHE") &
         file.exists(here::here("app/otherScripts/sendNotifications.txt"))) {
-      write(args["country"], file = here::here("app/otherScripts/notificationsToSend.txt"), append = TRUE)
+      write(args$country, file = here::here("app/otherScripts/notificationsToSend.txt"), append = TRUE)
     }
   } else {
-    cat(str_c(Sys.time(), " | ", args["country"], ": No new data available. Skipping Re calculation.\n"))
+    cat(str_c(Sys.time(), " | ", args$country, ": No new data available. Skipping Re calculation.\n"))
   }
 } else {
-  cat(str_c(Sys.time(), " | ", args["country"], ": No data available.\n"))
+  cat(str_c(Sys.time(), " | ", args$country, ": No data available.\n"))
 }
